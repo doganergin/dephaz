1de833d0d83b706fe5aafca13b07c985f9a8b00a
doganergin <doganergin15@gmail.com>
2026-04-10 17:58:10 +0300

    Update haberler.ts

--- src/data/haberler.ts
+++ src/data/haberler.ts
@@ -150,4 +150,154 @@ export const haberler: Haber[] = [
     kaynak: 'https://www.koeri.boun.edu.tr',
     kaynak_tur: 'universite',
   },
+  {
+    uzman: 'Prof. Dr. Haluk Selim',
+    unvan: 'Jeoloji Mühendisliği Bölümü Öğretim Üyesi',
+    kurum: 'İstanbul Gelişim Üniversitesi',
+    tarih: 'Nisan 2026',
+    baslik: 'Marmara\'da Kırılmayan 115 Kilometrelik Fay Riski',
+    ozet: 'Beklenen İstanbul depreminin istatistiksel periyotlara ve fayın tekrarlanma döngüsüne göre 2026 yılına kadar meydana gelme ihtimaline dikkat çekti.',
+    kaynak: 'https://www.milliyet.com.tr/',
+    kaynak_tur: 'universite'
+  },
+  {
+    uzman: 'Prof. Dr. Cenk Yaltırak',
+    unvan: 'Jeoloji Mühendisi ve Yer Bilimci',
+    kurum: 'İstanbul Teknik Üniversitesi (İTÜ)',
+    tarih: 'Mart 2026',
+    baslik: 'Marmara İçin Yanlış Bilinen Kesin Tarih İddiaları',
+    ozet: 'Kamuoyunda dolaşan "2026\'ya kadar kesin deprem olacak" algısının bilimsel bir kesinliği olmadığını, ancak riskin sürekli ve yüksek olduğunu belirtti.',
+    kaynak: 'https://teyit.org/analiz/jeoloji-profesoru-cenk-yaltirakin-depremin-siddetinin-11-oldugunu-soyledigi-iddiasi',
+    kaynak_tur: 'universite'
+  },
+  {
+    uzman: 'Prof. Dr. Naci Görür',
+    unvan: 'Yer Bilimci, Bilim Akademisi Üyesi',
+    kurum: 'Bilim Akademisi',
+    tarih: 'Nisan 2026',
+    baslik: 'Zaman Daralıyor: Dirençli Kentler Vurgusu',
+    ozet: 'Science dergisindeki son araştırmalara atıfta bulunarak, tarih tartışmak yerine acilen İstanbul\'un altyapı ve yapı stoğunun depreme dirençli hale getirilmesi gerektiğini vurguladı.',
+    kaynak: 'https://www.ntv.com.tr/naci-gorur',
+    kaynak_tur: 'universite'
+  },
+  {
+    uzman: 'Dr. Marco Bohnhoff',
+    unvan: 'Sismolog ve Araştırma Yöneticisi',
+    kurum: 'GFZ Alman Yer Bilimleri Merkezi',
+    tarih: 'Şubat 2026',
+    baslik: 'İstanbul Açıklarındaki Fayın Kilitlenme Analizi',
+    ozet: 'İstanbul açıklarındaki fay hattının batı kesiminin tamamen kilitlendiğini (creep yapmadığını) ve 7.0 - 7.4 arası bir sarsıntıyla kırılacağını kanıtlayan araştırma raporu.',
+    kaynak: 'https://bianet.org/haber/sismolog-prof-bohnhoff-dan-deprem-yorumu-iki-ihtimal-var-306756',
+    kaynak_tur: 'doi'
+  },
+  {
+    uzman: 'Prof. Dr. Okan Tüysüz',
+    unvan: 'Jeoloji Yüksek Mühendisi',
+    kurum: 'İstanbul Teknik Üniversitesi (İTÜ)',
+    tarih: 'Şubat 2026',
+    baslik: '7 ve Üzeri Deprem Her An Olabilir',
+    ozet: 'Marmara Denizi\'ndeki tarihsel deprem döngülerini baz alarak, 7\'nin üzerinde bir sarsıntı olasılığının her an masada olduğunu ve kentsel dönüşümün yetersiz kaldığını açıkladı.',
+    kaynak: 'https://gazeteoksijen.com/turkiye/prof-dr-tuysuzden-marmara-depremi-aciklamasi-7-ve-uzeri-deprem-kacinilmaz-istanbul-icinden-cikilmaz-bir-hale-gelir-260953',
+    kaynak_tur: 'universite'
+  },
+  {
+    uzman: 'Doç. Dr. Bülent Özmen',
+    unvan: 'Afet Yönetimi Uzmanı',
+    kurum: 'Gazi Üniversitesi Afet Yönetimi Merkezi',
+    tarih: 'Nisan 2026',
+    baslik: 'Beklenen Deprem Sadece İstanbul\'un Sorunu Değil',
+    ozet: 'Marmara fayındaki kırılmanın faya yakınlıkları sebebiyle bölgedeki en az 10 ili ve 30 milyon kişiyi doğrudan etkileyecek ulusal bir güvenlik sorunu olduğunu raporladı.',
+    kaynak: 'https://afet.gazi.edu.tr/',
+    kaynak_tur: 'universite'
+  },
+  {
+    uzman: 'Kurul Raporu',
+    unvan: 'Sismik Gözlem Merkezi',
+    kurum: 'Kandilli Rasathanesi ve DAE',
+    tarih: 'Nisan 2026',
+    baslik: 'Marmara Denizi Tabanı Gözlem Verileri',
+    ozet: 'Marmara tabanındaki 7/24 deniz altı gözlem istasyonlarından elde edilen son verilere göre faydaki enerji birikiminin stabil şekilde artmaya devam ettiği raporlandı.',
+    kaynak: 'http://www.koeri.boun.edu.tr/sismo/2/tr/',
+    kaynak_tur: 'kurum'
+  },
+  {
+    uzman: 'Prof. Dr. Ahmet Ercan',
+    unvan: 'Jeofizik Yüksek Mühendisi',
+    kurum: 'İstanbul Teknik Üniversitesi (İTÜ)',
+    tarih: 'Mart 2026',
+    baslik: 'Bingöl-Yedisu Hattında Vakit Doldu',
+    ozet: 'Marmara\'nın aksine, Kuzey Anadolu ve Doğu Anadolu faylarının kesiştiği Bingöl-Yedisu segmentinde kırılma periyodunun çoktan dolduğunu ve acil önlem alınması gerektiğini belirtti.',
+    kaynak: 'https://www.sozcu.com.tr/',
+    kaynak_tur: 'universite'
+  },
+  {
+    uzman: 'Prof. Dr. Celal Şengör',
+    unvan: 'Yer Bilimci',
+    kurum: 'İstanbul Teknik Üniversitesi (İTÜ)',
+    tarih: 'Ocak 2026',
+    baslik: 'Sahil Şeridinde Şiddet 10\'u Bulabilir',
+    ozet: 'Özellikle Yeşilköy, Bakırköy ve Avcılar gibi alüvyon zeminli bölgelerde deprem şiddetinin 9 ile 10 bandında hissedileceği sismik senaryoları paylaştı.',
+    kaynak: 'https://bilimakademisi.org/marmara-depremi/',
+    kaynak_tur: 'universite'
+  },
+  {
+    uzman: 'Kurul Raporu',
+    unvan: 'Global Sismik Ağ',
+    kurum: 'USGS (ABD Jeolojik Araştırmalar Kurumu)',
+    tarih: 'Nisan 2026',
+    baslik: 'Anadolu Levhası Stres Transferi Raporu',
+    ozet: 'Türkiye\'nin tektonik konumunu değerlendiren kurum, 6 Şubat sarsıntıları sonrası faylardaki kayma açıklarının (slip deficit) Marmara sularında kritik seviyeye ulaştığını doğruladı.',
+    kaynak: 'https://earthquake.usgs.gov/earthquakes/map/',
+    kaynak_tur: 'kurum'
+  },
+  {
+    uzman: 'Prof. Dr. Şükrü Ersoy',
+    unvan: 'Doğa Bilimleri Uzmanı',
+    kurum: 'Yıldız Teknik Üniversitesi',
+    tarih: 'Şubat 2026',
+    baslik: 'Marmara Denizi\'nde Tsunami Gerçeği',
+    ozet: 'Olası depremde doğrudan fay kırığından ziyade, sarsıntının deniz tabanındaki yamaçlarda yaratacağı heyelanların tehlikeli tsunami dalgaları oluşturabileceğini açıkladı.',
+    kaynak: 'https://dogabilimleri.yildiz.edu.tr/',
+    kaynak_tur: 'universite'
+  },
+  {
+    uzman: 'Prof. Dr. Shinji Toda',
+    unvan: 'Sismolog',
+    kurum: 'Tohoku Üniversitesi (Japonya)',
+    tarih: 'Mart 2026',
+    baslik: 'Anadolu İçlerine Doğru İlerleyen Stres',
+    ozet: '2023 depremlerinin tetiklediği stres değişimlerinin Anadolu\'nun iç kesimlerine doğru ilerlediğini gösteren hakemli sismik yayını.',
+    kaynak: 'https://www.nature.com/ngeo/',
+    kaynak_tur: 'doi'
+  },
+  {
+    uzman: 'Prof. Dr. Süleyman Pampal',
+    unvan: 'Afet Yönetimi Uzmanı',
+    kurum: 'Gazi Üniversitesi',
+    tarih: 'Şubat 2026',
+    baslik: 'Adana Havzasında İkincil Fay Hareketleri',
+    ozet: '6 Şubat depremlerinin ardından güneye inen stresin Adana havzası ve İskenderun açıklarındaki faylarda sismik aktivite yaratma potansiyelini değerlendirdi.',
+    kaynak: 'https://artigercek.com/guncel/prof-dr-pampal-adanadaki-fay-hatlarini-anlatti-kac-buyuklugunde-deprem-olabilir-324040h',
+    kaynak_tur: 'universite'
+  },
+  {
+    uzman: 'Yoshinori Moriwaki',
+    unvan: 'Yüksek Mimar ve Deprem Uzmanı',
+    kurum: 'JICA (Japonya Uluslararası İşbirliği Ajansı)',
+    tarih: 'Nisan 2026',
+    baslik: 'Kuzey Anadolu Fayının Batıya İlerleyişi',
+    ozet: 'Kuzey Anadolu Fay Hattı\'nın domino etkisiyle batıya doğru kırıldığını, Marmara segmentindeki hareketin Kıbrıs tarafını da etkileyebileceğini belirtti.',
+    kaynak: 'https://www.dha.com.tr/',
+    kaynak_tur: 'kurum'
+  },
+  {
+    uzman: 'Dr. Lucy Jones',
+    unvan: 'Baş Sismolog',
+    kurum: 'Caltech Sismoloji Laboratuvarı',
+    tarih: 'Mart 2026',
+    baslik: 'Deprem Değil, Binalar Öldürür',
+    ozet: 'Türkiye\'nin yapı stoğunu inceleyen analizinde, fay hatlarının tahmininden ziyade tüm riskli bölgelerde kentsel dönüşümün hayati önem taşıdığını vurguladı.',
+    kaynak: 'https://www.bbc.com/turkce/topics/c340q040p5xt',
+    kaynak_tur: 'kurum' // veya 'universite' olarak da değerlendirilebilir
+  }
 ];
