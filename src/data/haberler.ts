fb28003257677f4dfcae869e4a50dedc797c1aa1
doganergin <doganergin15@gmail.com>
2026-06-02 16:09:37 +0300

    Update haberler.ts

--- src/data/haberler.ts
+++ src/data/haberler.ts
@@ -341,4 +341,14 @@ export const haberler: Haber[] = [
     kaynak: 'https://doi.org/10.1126/science.288.5466.661',
     kaynak_tur: 'doi'
   }
+  {
+  uzman: 'Prof. Dr. Naci Görür',
+  unvan: 'Yer Bilimci',
+  kurum: 'İTÜ / Bilim Akademisi',
+  tarih: 'Güncel',
+  baslik: 'Akoluk Depremleri ve Adana Havzasına İlerleyen Sismik Aktivite',
+  ozet: 'Akoluk\'ta yaşanan 4.0 ve 4.9 büyüklüğündeki depremlerin Savrun ile Saimbeyli Fayları arasında, DAF üzerinde olduğu; depremlerin Adana Havzasına doğru ilerlediği ve 2023 depremlerinin stres birikiminde etkisi olduğu vurgulanarak "Deprem Dirençli Adana" çağrısı yapıldı.',
+  kaynak: 'https://x.com/nacigorur/status/2059019497747382422',
+  kaynak_tur: 'sosyal_medya'
+}
 ];
